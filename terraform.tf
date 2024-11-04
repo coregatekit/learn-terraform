17e22199b45f190785df955dd963465e9e8df5f2
Joe Kim <joesm.genie@gmail.com>
2024-11-04 23:42:47 +0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "coregatekit"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
